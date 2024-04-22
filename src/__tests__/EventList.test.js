abf9412657d7309059389a294ebc475a9edb7545
RParker505 <rchiado75@gmail.com>
2024-04-22 06:17:17 -0600

    Add id keys to array objects

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -13,7 +13,7 @@ describe('<EventList /> component', () => {
 
     //test will pass if list renders with 4 listitem elements
     test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+    const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 
