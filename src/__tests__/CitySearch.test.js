a5c63477df68686cfe367c6d4daa25a91b6a1360
RParker505 <rchiado75@gmail.com>
2024-04-22 16:49:10 -0600

    Add tests for hiding suggestion list by default and loading them when user types in textbox

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,6 +1,7 @@
 // src/__tests__/CitySearch.test.js
 
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 
 describe('<CitySearch /> component', () => {
@@ -11,6 +12,24 @@ describe('<CitySearch /> component', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
-      });
-      
+    });
+
+    //Ensure suggestion list is hidden by default
+    test('suggestions list is hidden by default', () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+    });
+
+    //Test that suggestion list appears when city input field gains focus
+    test('renders a list of suggestions when city textbox gains focus', async () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.click(cityTextBox);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass('suggestions');
+    });
+
 });
