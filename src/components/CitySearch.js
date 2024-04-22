fe1a5426f39aad89708bd57fb80d210010c5553f
RParker505 <rchiado75@gmail.com>
2024-04-22 16:56:33 -0600

    Create a local state of hiding/showing suggestion list

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,11 @@
 // src/components/CitySearch.js
 
+import { useState } from "react";
+
 const CitySearch = () => {
+    //showSuggestions is false by default - hide suggestion list to start
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    
     return (
       <div id="city-search">
         <input
@@ -8,6 +13,8 @@ const CitySearch = () => {
             className="city"
             placeholder="Search for a city"
         />
+        {/* if showSuggestions is true, render a ul element with a suggestions list. Otherwise, render a null */}
+        {showSuggestions ? <ul className="suggestions"></ul> : null}
       </div>
     )
    }
