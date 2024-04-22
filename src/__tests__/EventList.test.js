e810aa9bcc3dbcfac804b7aea238eada233a4ecd
RParker505 <rchiado75@gmail.com>
2024-04-22 16:26:51 -0600

    Refactor test suite to utilize beforeEach()
    Clean up duplicate code that renders the EventList

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -4,17 +4,21 @@ import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
-  
+
+    let EventListComponent;
+    beforeEach(() => {
+      EventListComponent = render(<EventList />);
+    })
+    
     //test will pass if EventList renders with a list
     test('has an element with "list" role', () => {
-    const EventListComponent = render(<EventList />);
-    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+      expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
     //test will pass if list renders with 4 listitem elements
     test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+      EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+      expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 
 });
