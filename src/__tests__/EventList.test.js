7390364a419a6be03d15e286a818c4718fa204d8
RParker505 <rchiado75@gmail.com>
2024-04-22 06:02:18 -0600

    Add test for event list length

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -4,8 +4,17 @@ import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
-  test('has an element with "list" role', () => {
+  
+    //test will pass if EventList renders with a list
+    test('has an element with "list" role', () => {
     const EventListComponent = render(<EventList />);
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
+
+    //test will pass if list renders with 4 listitem elements
+    test('renders correct number of events', () => {
+    const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+  });
+
 });
