fcee02977623c9af1ee5a4cf6ff9a9272016770e
RParker505 <rchiado75@gmail.com>
2024-04-23 16:26:26 -0600

    Replace simple objects with mockData used in getEvents

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,6 +2,7 @@
 
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
+import { getEvents } from '../api';
 
 describe('<EventList /> component', () => {
 
@@ -16,9 +17,10 @@ describe('<EventList /> component', () => {
   });
 
     //test will pass if list renders with 4 listitem elements
-    test('renders correct number of events', () => {
-      EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-      expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+    test('renders correct number of events', async () => {
+      const allEvents = await getEvents();
+      EventListComponent.rerender(<EventList events={allEvents} />);
+      expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
 
 });
