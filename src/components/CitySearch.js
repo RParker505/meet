8b3010a0f2fe672f8f2a53ab11cbcbf2657a8ff7
RParker505 <rchiado75@gmail.com>
2024-04-23 06:19:57 -0600

    Update code to filter suggestions based on user input

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -2,9 +2,23 @@
 
 import { useState } from "react";
 
-const CitySearch = () => {
+const CitySearch = (allLocations) => {
     //showSuggestions is false by default - hide suggestion list to start
     const [showSuggestions, setShowSuggestions] = useState(false);
+    //set state of input field so you can access its value
+    const [query, setQuery] = useState("");
+    //suggestions will hold the list of suggestions
+    const [suggestions, setSuggestions] = useState([]);
+
+    const handleInputChanged = (event) => {
+      const value = event.target.value;
+      const filteredLocations = allLocations ? allLocations.filter((location) => {
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+      }) : [];
+  
+      setQuery(value);
+      setSuggestions(filteredLocations);
+    };
     
     return (
       <div id="city-search">
@@ -12,10 +26,22 @@ const CitySearch = () => {
             type="text"
             className="city"
             placeholder="Search for a city"
+            value={query}
             onFocus={() => setShowSuggestions(true)}
+            onChange={handleInputChanged}
         />
         {/* if showSuggestions is true, render a ul element with a suggestions list. Otherwise, render a null */}
-        {showSuggestions ? <ul className="suggestions"></ul> : null}
+        {showSuggestions ?
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => {
+            return <li key={suggestion}>{suggestion}</li>
+          })}
+          <li key='See all cities'>
+            <b>See all cities</b>
+          </li>
+        </ul>
+        : null
+        }
       </div>
     )
    }
