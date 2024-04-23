6052b4573ec106c4d455a80c257abbaa31491bbb
RParker505 <rchiado75@gmail.com>
2024-04-23 17:08:38 -0600

    Add placeholder for tests needed for Feature 2

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,4 +1,24 @@
 import { render } from '@testing-library/react';
 import Event from '../components/Event';
 
-describe('<Event /> component', () => {}
+describe('<Event /> component', () => {
+
+    //Test that event element is rendered
+
+    //Test that event details are not visible on initial render
+
+    //Test that clicking on event element triggers state change or class change to expand element
+
+    //Test that event details are visible after element click
+
+    //Test that icons/text changes in expanded state display correctly
+
+    //Test that clicking element in expanded state triggers state change or class change to collapse element
+
+    //Test that event details are hidden after event is clicked
+
+    //Test that icons/text changes revert after collapse
+
+    // If toggle, check that toggle changes state correctly
+
+}
