d446d9e091a37cb84471dc08936a1ccf89bd8c45
RParker505 <rchiado75@gmail.com>
2024-04-25 05:38:56 -0600

    Write test to render number of events

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,4 +18,8 @@ describe('<App /> component', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
     });
 
+  test('render Number of Events', () => {
+    expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
+    });
+
 });
