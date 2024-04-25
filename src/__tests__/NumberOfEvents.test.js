0ee64c24c43d8a80470b6366a804ab112dd4c2c4
RParker505 <rchiado75@gmail.com>
2024-04-25 06:18:48 -0600

    Write test to ensure textbox value changes as user types

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,6 +1,7 @@
 // src/__tests__/NumberOfEvents.test.js
 
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import NumberOfEvents from '../components/NumberOfEvents';
 
 describe('<NumberOfEvents /> component', () => {
@@ -20,6 +21,14 @@ describe('<NumberOfEvents /> component', () => {
     test('defaults to 32 events', () => {
         const eventNumberBox = NumberOfEventsComponent.queryByRole('textbox');
         expect(eventNumberBox).toHaveValue('32');
-    })
+    });
+
+    //check whether event count changes when user types
+    test('value changes as user types in textbox', async () => {
+        const user = userEvent.setup();
+        const eventNumberBox = NumberOfEventsComponent.queryByRole('textbox');
+        await user.type(eventNumberBox, '{backspace}{backspace}10');
+        expect(eventNumberBox).toHaveValue('10');
+    });
 
 })
