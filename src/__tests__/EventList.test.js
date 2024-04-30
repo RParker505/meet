4b2097ea870192a28abd9f714223eb56aa53559e
RParker505 <rchiado75@gmail.com>
2024-04-30 05:33:45 -0600

    Add integration test for rendering event list when app mounts

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,9 +1,11 @@
 // src/__tests__/EventList.test.js
 
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import EventList from '../components/EventList';
+import App from '../App';
 import { getEvents } from '../api';
 
+//scope for unit tests
 describe('<EventList /> component', () => {
 
     let EventListComponent;
@@ -22,5 +24,20 @@ describe('<EventList /> component', () => {
       EventListComponent.rerender(<EventList events={allEvents} />);
       expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
+
+});
+
+//new scope for integration testing
+describe('<EventList /> integration', () => {
+
+    test('renders a list of 32 events when the app is mounted and rendered', async () => {
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
+      const EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(EventListItems.length).toBe(32);
+      });
+    });
 
 });
