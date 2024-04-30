1edf90dd0195f6839f0ac0b52b606ac7bcebaf7b
RParker505 <rchiado75@gmail.com>
2024-04-30 06:01:59 -0600

    Create/initialize allLocations state and pass it to CitySearch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents } from "./api";
+import { extractLocations, getEvents } from "./api";
 
 import './App.css';
 
@@ -13,19 +13,21 @@ const App = () => {
 
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
-
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
-  }
+  const [allLocations, setAllLocations] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} />
       <NumberOfEvents />
       <EventList events={events} />
     </div>
