8404c70389f34615a5f01b210d5c85ecbd17cb9a
RParker505 <rchiado75@gmail.com>
2024-05-04 11:58:27 -0600

    Add logic for Scenario 2 to pass test

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,5 +1,6 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, within, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { getEvents } from '../api';
 
@@ -28,16 +29,23 @@ defineFeature(feature, test => {
     });
 
     test('user should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
+        let AppComponent;
         given('the main page is open', () => {
-    
+          AppComponent = render(<App />);
         });
     
-        when('user starts typing in the city textbox', () => {
-    
+        let CitySearchDOM;
+        when('user starts typing in the city textbox', async () => {
+          const user = userEvent.setup();
+          const AppDOM = AppComponent.container.firstChild;
+          CitySearchDOM = AppDOM.querySelector('#city-search');
+          const citySearchInput = within(CitySearchDOM).queryByRole('textbox');  
+          await user.type(citySearchInput, "Berlin");
         });
     
-        then('the user should receive a list of cities (suggestions) that match what they’ve typed.', () => {
-    
+        then('the user should receive a list of cities (suggestions) that match what they’ve typed.', async () => {
+          const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem'); 
+          expect(suggestionListItems).toHaveLength(2);
         });
       });
 
