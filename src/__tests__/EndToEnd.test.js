0f30133ae20abe98f211ea15eacf4e96387e4bbb
RParker505 <rchiado75@gmail.com>
2024-05-04 13:11:06 -0600

    Write end-to-end test for feature 2, scenario 3

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -18,12 +18,18 @@ describe('show/hide an event details', () => {
     test('An event element is collapsed by default', async () => {
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeNull();
-      });
+    });
 
-      test('User can expand an event to see its details', async () => {
+    test('User can expand an event to see its details', async () => {
         await page.click('.event .DetailsButton');
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeDefined();
-      });
+    });
+
+    test('User can collapse an event to hide details', async () => {
+        await page.click('.event .DetailsButton');
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();
+    });
 
 });
