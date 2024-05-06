8478a899dc5b497a1a019306448ec5a54ba1ca16
RParker505 <rchiado75@gmail.com>
2024-05-06 08:14:22 -0600

    Insert 3 scenario acceptance tests for Feature 2

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -4,4 +4,46 @@ const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
 
+    test('An event element is collapsed by default.', ({ given, when, then }) => {
+        given('the main page is open', () => {
+
+        });
+
+        when('the user is scrolling through events', () => {
+
+        });
+
+        then('event details are collapsed by default.', () => {
+
+        });
+    });
+
+    test('User can expand an event to see details.', ({ given, when, then }) => {
+        given('the user is viewing a list of events', () => {
+
+        });
+
+        when('the user clicks on an event\'s ‘show details’ button', () => {
+
+        });
+
+        then('the event details will expand.', () => {
+
+        });
+    });
+
+    test('User can collapse an event to hide details.', ({ given, when, then }) => {
+        given('the user is viewing expanded event details', () => {
+
+        });
+
+        when('the user clicks on ‘hide details’ button', () => {
+
+        });
+
+        then('the event details will collapse/hide.', () => {
+
+        });
+    });
+
 });
