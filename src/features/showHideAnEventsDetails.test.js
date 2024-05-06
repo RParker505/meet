fe299a4303a34b0655e17b867571c426c3fdf470
RParker505 <rchiado75@gmail.com>
2024-05-06 08:25:06 -0600

    Write logic for acceptance test for Feature 2, Scenario 1

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,20 +1,33 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
+import { render, within, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import Event from "../components/Event";
+import { getEvents } from '../api';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
 
     test('An event element is collapsed by default.', ({ given, when, then }) => {
+        let AppComponent;
         given('the main page is open', () => {
-
+            AppComponent = render(<App />);
         });
 
-        when('the user is scrolling through events', () => {
-
+        when('the user is scrolling through events', async () => {
+            const AppDOM = AppComponent.container.firstChild;
+            const EventListDOM = AppDOM.querySelector('#event-list');
+            await waitFor(() => {
+              const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+              expect(EventListItems.length).toBe(32);
+            });
         });
 
         then('event details are collapsed by default.', () => {
-
+            const AppDOM = AppComponent.container.firstChild;
+            const eventDetails = AppDOM.querySelector('.details');
+            expect(eventDetails).not.toBeInTheDocument();
         });
     });
 
