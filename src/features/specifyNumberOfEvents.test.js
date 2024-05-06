7ee3489ad59f889b5302b7f3665340f573961d70
RParker505 <rchiado75@gmail.com>
2024-05-06 08:57:00 -0600

    Write logic to pass Feature 3, scenario 1

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,20 +1,31 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
+import { render, within, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
 
     test('When user has not specified a number, 32 events are shown by default.', ({ given, when, then }) => {
+        let AppComponent;
         given('the user is viewing the main page', () => {
-
+            AppComponent = render(<App />);
         });
 
         when('the user has not specified a number of events', () => {
-
+            const AppDOM = AppComponent.container.firstChild;
+            const EventListDOM = AppDOM.querySelector('#event-list');
+            expect(EventListDOM).toBeInTheDocument();
         });
 
-        then(/^(\d+) events will be shown by default.$/, (arg0) => {
-
+        then(/^(\d+) events will be shown by default.$/, async (arg0) => {
+            const AppDOM = AppComponent.container.firstChild;
+            const EventListDOM = AppDOM.querySelector('#event-list');
+            await waitFor(() => {
+                const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+                expect(EventListItems.length).toBe(32);
+              });
         });
     });
 
