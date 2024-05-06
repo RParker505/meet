3f2e3dddfbe8fe2227937af1257e58036794b269
RParker505 <rchiado75@gmail.com>
2024-05-06 08:52:48 -0600

    Write basic acceptance tests for Feature 3 scenarios

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -4,4 +4,32 @@ const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
 
+    test('When user has not specified a number, 32 events are shown by default.', ({ given, when, then }) => {
+        given('the user is viewing the main page', () => {
+
+        });
+
+        when('the user has not specified a number of events', () => {
+
+        });
+
+        then(/^(\d+) events will be shown by default.$/, (arg0) => {
+
+        });
+    });
+
+    test('User can change the number of events displayed.', ({ given, when, then }) => {
+        given('the user is viewing the main page', () => {
+
+        });
+
+        when('the user selects a number of events to display', () => {
+
+        });
+
+        then('the app will display the selected number of events.', () => {
+
+        });
+    });
+
 });
