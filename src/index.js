16d5a583c5efa374031c009f2612e879a836d805
RParker505 <rchiado75@gmail.com>
2024-05-08 16:23:37 -0600

    Remove error notification since Atatus integration is working

--- src/index.js
+++ src/index.js
@@ -23,6 +23,3 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// Verify that Atatus integration is working
-atatus.notify(new Error('Test Atatus Setup'));
