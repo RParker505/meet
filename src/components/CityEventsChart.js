2c5ec06dbbbd37782ad2ca72425e62308e09518b
RParker505 <rchiado75@gmail.com>
2024-05-25 11:04:37 -0600

    Write function to get number of events in each city and shorten location

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -10,7 +10,18 @@ import {
   ResponsiveContainer
 } from 'recharts';
 
-const CityEventsChart = () => {
+const CityEventsChart = ({allLocations, events}) => {
+
+    //get number of events in each city and shorten each location to only the city
+    const getData = () => {
+        const data = allLocations.map((location) => {
+        const count = events.filter((event) => event.location === location).length
+          const city = location.split(', ')[0]
+          return { city, count };
+        })
+        return data;
+      };
+
 }
 
 export default CityEventsChart;
