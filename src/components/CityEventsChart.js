dad60b8a0a5d7faf5a1897be454641802861275d
RParker505 <rchiado75@gmail.com>
2024-05-25 11:30:08 -0600

    Increase bottom margin for chart to fit city names

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -34,7 +34,7 @@ const CityEventsChart = ({allLocations, events}) => {
             margin={{
               top: 20,
               right: 20,
-              bottom: 20,
+              bottom: 60,
               left: 20,
             }}
           >
