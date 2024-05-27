5d8088c20a23820989ce3e9de19ac8beb28cc943
RParker505 <rchiado75@gmail.com>
2024-05-27 11:09:54 -0600

    Create shell EventGenresChart component

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,9 @@
+// src/components/EventGenresChart.js
+
+import { useState, useEffect } from 'react';
+import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts';
+
+const EventGenresChart = () => {
+}
+
+export default EventGenresChart;
